d8e96d6f84c52ca424a56be1e1f3456ee38c62f9
abrar1993 <ch.abrar50@gmail.com>
2022-06-20 14:01:33 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from 'react';
+import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpenses/NewExpense';
 
-function App() {
+let DUMMY_EXPENSE=[
+  {
+    id:'e1',
+    title:'School Fee',
+    amount:250,
+    date:new Date(2022,6,10)
+  },
+  {
+    id:'e2',
+    title:'Health Insurance',
+    amount:500,
+    date:new Date(2022,6,5)
+  },
+  {
+    id:'e3',
+    title:'House Rent',
+    amount:1000,
+    date:new Date(2022,6,1)
+  },
+  {
+    id:'e4',
+    title:'Food',
+    amount:1250,
+    date:new Date(2022,6,15)
+  }
+];
+const App=()=> {
+  
+const [expenses,setExpenses]=useState(DUMMY_EXPENSE);
+
+useEffect(()=>{
+  fetch('https://techgun.website/sample/api/read.php').then(
+      response => {
+          return response.json();
+      }
+  ).then(
+      data => {
+          // console.log(data);
+          setExpenses(data);
+      }
+  );
+
+
+},[]);
+const addExpenseHandler=(expense)=>{
+  const updatedExpense=[expense,...expenses];
+  setExpenses(updatedExpense);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses item={expenses}/>
+      
     </div>
   );
 }
